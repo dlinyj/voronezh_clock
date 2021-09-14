f50c7c0b97098b838ed3347ce256673c18969f35
Sergei Dolin <dlinyj@gmail.com>
2021-09-15 00:23:52 +0300

    Финальный работающий вариант

--- voronezh.ino
+++ voronezh.ino
@@ -11,6 +11,7 @@ tmElements_t m_time = {0};
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);// RS E
 
 volatile boolean blink = true;
+volatile uint8_t numbers[6] 	={0};
 
 // keypad keys definitions
 enum KP
@@ -56,20 +57,20 @@ inline void set_nego_sig() __attribute__((always_inline));
 
 void setupTimer1() {
 	noInterrupts();
+	// Clear registers
 	TCCR1A = 0;
 	TCCR1B = 0;
 	TCNT1 = 0;
-	// 20000 Hz (16000000/((99+1)*8))
-	OCR1A = 99;
+
+	// 12003.000750187546 Hz (16000000/((1332+1)*1))
+	OCR1A = 1332;
 	// CTC
 	TCCR1B |= (1 << WGM12);
-	// Prescaler 8
-	TCCR1B |= (1 << CS11);
+	// Prescaler 1
+	TCCR1B |= (1 << CS10);
 	// Output Compare Match A Interrupt Enable
 	TIMSK1 |= (1 << OCIE1A);
 	interrupts();
-
-	ClockState = RUN;
 }
 
 void setup() {
@@ -117,16 +118,24 @@ boolean test_leap_year(uint8_t year)  {
 	}
 }
 
+void load_time () {
+	noInterrupts();
+	numbers[0]	= m_time.Hour / 10;
+	numbers[1]	= m_time.Hour % 10;
+	numbers[2]	= m_time.Minute / 10;
+	numbers[3]	= m_time.Minute % 10;
+	numbers[4]	= m_time.Second /10;
+	numbers[5]	= m_time.Second % 10;
+	interrupts();
+}
+
 uint8_t  settime () {
-	tmElements_t l_time = {0};
 	int set = 0;
 	const unsigned int month_days [] =
 		{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-	RTC.read(l_time); //read time from DS3231
-	noInterrupts();
-	m_time = l_time; //send time to output function
-	interrupts();
+	RTC.read(m_time); //read time from DS3231
+	load_time ();
 	//delay(200)
 	static unsigned long old_time = 0;
 	static unsigned long new_time;
@@ -151,42 +160,42 @@ uint8_t  settime () {
 		if (ClockState != RUN) {
 			if (key == KEY_RIGHT) {
 				set++;
-				l_time.Second = 0;
+				m_time.Second = 0;
 			}
 
 			if (key == KEY_UP) {
 				switch (ClockState) {
 					case SETHOUR:
 						set++;
-						l_time.Hour++;
-						if (l_time.Hour > 23) l_time.Hour = 0;
+						m_time.Hour++;
+						if (m_time.Hour > 23) m_time.Hour = 0;
 						break;	
 					case SETMINUTE:
 						set++;
-						l_time.Minute++;
-						if (l_time.Minute > 59) l_time.Minute = 0;
+						m_time.Minute++;
+						if (m_time.Minute > 59) m_time.Minute = 0;
 						break;
 					case SETDAY:
 						set++;
-							if ((l_time.Month != 2 && l_time.Day < month_days[l_time.Month])
+							if ((m_time.Month != 2 && m_time.Day < month_days[m_time.Month])
 								||
-								(l_time.Month == 2 && l_time.Day < 28)
+								(m_time.Month == 2 && m_time.Day < 28)
 								||
-								(l_time.Month == 2 && l_time.Day == 28 &&
-								test_leap_year(l_time.Year))) {
-									l_time.Day++;
+								(m_time.Month == 2 && m_time.Day == 28 &&
+								test_leap_year(m_time.Year))) {
+									m_time.Day++;
 							}
 						break;
 					case SETMONTH:
 						set++;
-						l_time.Month++;
-						if (l_time.Month > 12) {
-							l_time.Month = 1;
+						m_time.Month++;
+						if (m_time.Month > 12) {
+							m_time.Month = 1;
 						}
 						break;
 					case SETYEAR:
 						set++;
-						l_time.Year++;
+						m_time.Year++;
 						break;
 				}
 			}
@@ -194,61 +203,59 @@ uint8_t  settime () {
 				switch (ClockState) {
 					case SETHOUR:
 						set++;
-						if (l_time.Hour == 0) {
-							l_time.Hour = 23;
+						if (m_time.Hour == 0) {
+							m_time.Hour = 23;
 						} else {
-							l_time.Hour--;
+							m_time.Hour--;
 						}
 						break;
 					case SETMINUTE:
 						set++;
-						if (l_time.Minute == 0) {
-							l_time.Minute = 59;
+						if (m_time.Minute == 0) {
+							m_time.Minute = 59;
 						} else {
-							l_time.Minute--;
+							m_time.Minute--;
 						}
 						break;
 					case SETDAY:
 						set++;
-						if (l_time.Day > 1) {
-							l_time.Day--;
+						if (m_time.Day > 1) {
+							m_time.Day--;
 						}
 						break;
 					case SETMONTH:
 						set++;
-						if (l_time.Month > 1) {
-							l_time.Month--;
+						if (m_time.Month > 1) {
+							m_time.Month--;
 						} else {
-							l_time.Month = 12;
+							m_time.Month = 12;
 						}
 
 						break;
 					case SETYEAR:
 						set++;
-						if (l_time.Year > 0) {
-							l_time.Year--;
+						if (m_time.Year > 0) {
+							m_time.Year--;
 						}
 						break;
 				}
 			}
 
 			if (set) {
-				if (l_time.Month != 2 && l_time.Day > month_days[l_time.Month]) {
-					l_time.Day = month_days[l_time.Month];
+				if (m_time.Month != 2 && m_time.Day > month_days[m_time.Month]) {
+					m_time.Day = month_days[m_time.Month];
 				}
-				if (l_time.Month == 2) {
-					if (test_leap_year(l_time.Year) && l_time.Day >= 29) {
-						l_time.Day = 29;
+				if (m_time.Month == 2) {
+					if (test_leap_year(m_time.Year) && m_time.Day >= 29) {
+						m_time.Day = 29;
 					} else {
-						if (l_time.Day >= 28) {
-							l_time.Day = 28;
+						if (m_time.Day >= 28) {
+							m_time.Day = 28;
 						}
 					}
 				}
-				noInterrupts();
-				m_time = l_time; //send time to timer function
-				interrupts();
-				return RTC.write(l_time);
+				load_time ();
+				return RTC.write(m_time);
 			}
 		}
 	} else {
@@ -358,135 +365,43 @@ void loop() {
 	print_the_time ();
 }
 
-#define PULSE_DURATION	100
-#define TIMER_PERIOD	50
-#define DELAY_AFTER_PULSE_TRAIN	1600
-
-#define H_SYNC_PULSE	0
-#define L_SYNC_PULSE	1
-#define NUM_SENDING	2 
-#define POS_SIG_NUM	3
-#define ZER_SIG_NUM	4
-
-#define SET_BIT(p,n) (p |= (1 << n))
-#define CLR_BIT(p,n) (p &= ~(1 << n))
-#define CHK_BIT(p,n) (p & (1 << n))
-
-
 void set_zero_sig() {
-//	digitalWrite(POS_SIG, HIGH);
-//	digitalWrite(NEG_SIG, HIGH);
 	PORTD |= (1 << POS_SIG) | (1 << NEG_SIG);
 }
 
 void set_posi_sig() {
-//	digitalWrite(POS_SIG, LOW);
-	PORTD &=~ (1 << POS_SIG);
+	PORTD = PORTD &~ (1 << POS_SIG) | (1 << NEG_SIG);
 }
 
 void set_nego_sig() {
-//	digitalWrite(NEG_SIG, LOW);
-	PORTD &=~ (1 << NEG_SIG);
+	PORTD = PORTD &~ (1 << NEG_SIG) | (1 << POS_SIG);
 }
 
 ISR(TIMER1_COMPA_vect) {
-	static int8_t numbers[6] 	={0};
-	static uint8_t number_send	= 0;
-	static uint8_t current_num	= 0;
-	static uint8_t pkg_stage	= 0;
-	static uint16_t _delay	 	= 0;
-	static uint8_t tenner		= 10;
-#ifdef DEBUG
-	static int exit_stage = 0;
-#endif
-	if (_delay >= TIMER_PERIOD) {
-#ifdef DEBUG
-		print_iterator ();
-#endif
-		_delay-=TIMER_PERIOD;
-		goto exit_isr;
-	} else {
-		_delay = 0;
-#ifdef DEBUG
-		if (exit_stage) exit(1);
-#endif
-	}
-	if (0 == pkg_stage) {
-		//SET_BIT(pkg_stage, H_SYNC_PULSE); //h-sync pulse stage
-		SET_BIT(pkg_stage, NUM_SENDING);
-		numbers[0]	= 10 - m_time.Hour / 10;
-		numbers[1]	= 10 - m_time.Hour % 10;
-		numbers[2]	= 10 - m_time.Minute / 10;
-		numbers[3]	= 10 - m_time.Minute % 10;
-		numbers[4]	= 10 - m_time.Second /10;
-		numbers[5]	= 10 - m_time.Second % 10;
-		//first pulse
-		set_nego_sig();
-		_delay 		= PULSE_DURATION;
-		goto exit_isr;
-	} else {
-		set_zero_sig();
-/*
-		if (CHK_BIT (pkg_stage, H_SYNC_PULSE)) {
-			CLR_BIT(pkg_stage, H_SYNC_PULSE);
-			SET_BIT(pkg_stage, L_SYNC_PULSE); //l-sync pulse stage
-			
-			set_posi_sig();
-			_delay = PULSE_DURATION;
-			goto exit_isr;
-		}
-		if (CHK_BIT (pkg_stage, L_SYNC_PULSE)) {
-			CLR_BIT(pkg_stage, L_SYNC_PULSE);
-			SET_BIT(pkg_stage, NUM_SENDING);
-			set_nego_sig();
-			_delay = PULSE_DURATION;
-			goto exit_isr;
-		}
-*/
-		if (CHK_BIT (pkg_stage, NUM_SENDING)) {
-			if(!(CHK_BIT (pkg_stage, POS_SIG_NUM)) && !(CHK_BIT (pkg_stage, ZER_SIG_NUM))) {
-				number_send = numbers[current_num];
-				tenner = 10;
-				SET_BIT(pkg_stage, POS_SIG_NUM);
-			}
-			if (0 != tenner) {
-				if (CHK_BIT (pkg_stage, POS_SIG_NUM)) {
-					CLR_BIT(pkg_stage, POS_SIG_NUM);
-					SET_BIT(pkg_stage, ZER_SIG_NUM);
-					set_posi_sig();
-					_delay = PULSE_DURATION;
-					goto exit_isr;
-				}
-				if (CHK_BIT (pkg_stage, ZER_SIG_NUM)) {
-					CLR_BIT(pkg_stage, ZER_SIG_NUM);
-					SET_BIT(pkg_stage, POS_SIG_NUM);
-					tenner--;
-					numbers[current_num]--;
-					if (numbers[current_num] == 0) {
-						set_nego_sig();
-					} else {
-						set_zero_sig();
-					}
-					_delay = PULSE_DURATION;
-					goto exit_isr;
-				}
+	static uint8_t frame_counter = 0;
+	static uint8_t cur_pos_num = 0;
+	if (frame_counter < 20) {
+		if (frame_counter & 1) {
+			if (numbers[cur_pos_num] == (9 - (frame_counter >> 1))) {
+				set_nego_sig();
 			} else {
-				_delay = DELAY_AFTER_PULSE_TRAIN;
 				set_zero_sig();
-				if (current_num < 5) {
-					current_num++;
-					tenner = 10;
-					//pkg_stage = (1 << H_SYNC_PULSE);
-				} else {
-					current_num = 0;
-					pkg_stage = 0;
-#ifdef DEBUG
-					exit_stage = 1;
-#endif
-				}
 			}
+		} else {
+			set_posi_sig();
 		}
 	}
-exit_isr:
-	return;
+	if (frame_counter++ == 39) {
+		frame_counter = 0;
+		if (cur_pos_num < 5) {
+			cur_pos_num++;
+		} else {
+			set_nego_sig(); 
+			cur_pos_num = 0;
+		}
+	} else {
+		if (frame_counter > 20) {
+			set_zero_sig();
+		}
+	}
 }
